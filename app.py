7aeebdcdfba50f10ce18e13c63fdb62fc3b9157e
Harinath-0704 <m.harinath0103@gmail.com>
2025-11-25 15:48:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,17 @@
 from flask import Flask, send_from_directory
+import os
 
 app = Flask(__name__, static_folder='static')
 
 @app.route('/')
-def serve_home():
+def index():
     return send_from_directory('static', 'index.html')
 
 @app.route('/<path:path>')
-def serve_static_files(path):
+def static_proxy(path):
+    # serve static files
     return send_from_directory('static', path)
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
